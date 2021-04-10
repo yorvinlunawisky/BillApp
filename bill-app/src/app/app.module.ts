f30784118f2cf6d251455e92ebc1681140f523d8
Yorvin Luna Wisky <yorvin0506@gmail.com>
2021-04-09 23:37:36 -0400

    Header added.

--- bill-app/src/app/app.module.ts
+++ bill-app/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ItemsComponent } from './items/items.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ItemsComponent
+    ItemsComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
